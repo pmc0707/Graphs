fca566281740a1242dd0c622ab54921345f88b17
pmc0707 <pranshugoodboy@gmail.com>
2024-04-05 11:42:15 +0530

    in this we use FIFO DS and go to immediate neighbour first

---
+++ src/graphTraversal/BreadthFirstSearch.java
@@ -0,0 +1,77 @@
+package graphTraversal;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BreadthFirstSearch {
+    static class Edge{
+        int source;
+        int destination;
+        int weight;
+
+        public Edge(int source,int destination,int weight) {
+            this.source = source;
+            this.destination = destination;
+            this.weight = weight;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge>[] graph) {
+        for (int index = 0; index < graph.length; index++) {
+            graph[index] = new ArrayList<>();
+        }
+            graph[0].add(new Edge(0,1,1));
+            graph[0].add(new Edge(0,2,1));
+
+            graph[0].add(new Edge(1,0,1));
+            graph[0].add(new Edge(1,3,1));
+
+            graph[0].add(new Edge(2,0,1));
+            graph[0].add(new Edge(2,4,1));
+
+            graph[0].add(new Edge(3,1,1));
+            graph[0].add(new Edge(3,4,1));
+            graph[0].add(new Edge(3,5,1));
+
+            graph[0].add(new Edge(4,2,1));
+            graph[0].add(new Edge(4,3,1));
+            graph[0].add(new Edge(4,5,1));
+
+            graph[0].add(new Edge(5,3,1));
+            graph[0].add(new Edge(5,4,1));
+            graph[0].add(new Edge(5,6,1));
+            graph[0].add(new Edge(5,5,1));
+    }
+    public static void bfs(ArrayList<Edge> [] graph){
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[graph.length];
+        queue.add(0);
+        while (!queue.isEmpty()) {
+            int current = queue.remove();
+            if (!visited[current]) {
+                System.out.print(current+" ");
+                visited[current] = true;
+                for (int index = 0; index < graph[current].size(); index++) {
+                    Edge edge = graph[current].get(index);
+                    queue.add(edge.destination);
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        /*
+        1----3
+       /      \
+      0        5 --6
+      \       /
+       2-----4
+        */
+
+        int vertex = 7;
+        ArrayList<Edge> []graph = new ArrayList[vertex];
+        createGraph(graph);
+        bfs(graph);
+    }
+
+}
